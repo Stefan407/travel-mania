4b1f56220dfe2dec04c5c318801ea47cc7b37071
Stefan <dev_1@studiosdl.com>
2020-07-10 21:00:13 +0300

    pull

--- wp-content/themes/lz-computer-repair/main.php
+++ wp-content/themes/lz-computer-repair/main.php
@@ -66,6 +66,26 @@
                 <a href="<?= home_url() ?>/all-cities/"> ВСЕ ГОРОДА</a>
                 <a href="<?= home_url() ?>/all-countries/"> ВСЕ СТРАНЫ</a>
             </div>
+<!-- Yandex.RTB R-A-602403-1 -->
+<div id="yandex_rtb_R-A-602403-1"></div>
+<script type="text/javascript">
+    (function(w, d, n, s, t) {
+        w[n] = w[n] || [];
+        w[n].push(function() {
+            Ya.Context.AdvManager.render({
+                blockId: "R-A-602403-1",
+                renderTo: "yandex_rtb_R-A-602403-1",
+                async: true
+            });
+        });
+        t = d.getElementsByTagName("script")[0];
+        s = d.createElement("script");
+        s.type = "text/javascript";
+        s.src = "//an.yandex.ru/system/context.js";
+        s.async = true;
+        t.parentNode.insertBefore(s, t);
+    })(this, this.document, "yandexContextAsyncCallbacks");
+</script>          
         </div>
     </section>
 
