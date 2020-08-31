ec203397237d3e7687b540440602b0dcb2ee3a59
Stefan <dev_1@studiosdl.com>
2020-08-31 09:48:42 +0300

    Новая версия сайта

--- inc/head-static.php
+++ inc/head-static.php
@@ -1,23 +1,3 @@
-<!-- Google Tag Manager -->
-<script data-ad-client="ca-pub-1224554491202001" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-<script>
-    (function(w, d, s, l, i) {
-        w[l] = w[l] || [];
-        w[l].push({
-            'gtm.start': new Date().getTime(),
-            event: 'gtm.js'
-        });
-        var f = d.getElementsByTagName(s)[0],
-            j = d.createElement(s),
-            dl = l != 'dataLayer' ? '&l=' + l : '';
-        j.async = true;
-        j.src =
-            'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
-        f.parentNode.insertBefore(j, f);
-    })(window, document, 'script', 'dataLayer', 'GTM-KH5TPRC');
-</script>
-<!-- End Google Tag Manager -->
-
 <link rel='stylesheet' href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap">
 <link rel='stylesheet' type="text/css" href="/assets/css/slick.min.css?v=3.8">
 <link rel='stylesheet' type="text/css" href="/assets/css/simplebar.min.css?v=3.8">
