3a8f4b181be3855374a686157bb349dd09e46696
StefanLenovo310 <stefanv407@gmail.com>
2020-10-15 22:32:36 +0300

    закоментировал вызов куки

--- inc/head-static.php
+++ inc/head-static.php
@@ -1,4 +1,12 @@
 
+<!-- Google Tag Manager -->
+<!-- <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-T8STP25');</script> -->
+<!-- End Google Tag Manager -->
+
 <link rel='stylesheet' href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap">
 <link rel='stylesheet' type="text/css" href="/assets/css/slick.min.css?v=7.1">
 <link rel='stylesheet' type="text/css" href="/assets/css/simplebar.min.css?v=7.1">
